ab372d1631244c8296182b0098e7aef7e1c113cb
Ivan Artamonov <Vanya200373@yandex.ru>
2025-06-15 21:16:44 +0300

    Fix user mistakes

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,8 +5,10 @@ from django.utils.translation import gettext_lazy
 
 class MyUser(AbstractUser):
     email = models.EmailField(gettext_lazy('email address'),
-                              blank=True,
+                              blank=False,
                               unique=True)
+    first_name = models.CharField('Имя', max_length=150, blank=False)
+    last_name = models.CharField('Фамилия', max_length=150, blank=False)
     avatar = models.ImageField(
         "Аватар", upload_to="users/",
         blank=True, null=True
