429068ac4dce010882730f5b4e5a8cd6a26c2048
Ivan Artamonov <Vanya200373@yandex.ru>
2025-06-16 00:21:51 +0300

    Admin page localization part 2

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
+from django.db.models import Count
 from .models import (Recipe, Ingredient, RecipeIngredient,
                      Favorite, ShoppingCart)
 
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
+    readonly_fields = ('favorites_count',)
     search_fields = ('name', 'author__email',
                      'author__first_name', 'author__last_name')
 
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.annotate(favorites_count=Count('favorite'))
+        return qs
 
-admin.site.register((Ingredient, RecipeIngredient,
-                     Favorite, ShoppingCart))
+    def favorites_count(self, obj):
+        return obj.favorites_count
+    favorites_count.short_description = 'В избранном'
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+admin.site.register((RecipeIngredient, Favorite, ShoppingCart))
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -51,7 +51,7 @@ class Ingredient(models.Model):
         verbose_name_plural = 'Ингредиенты'
 
     def __str__(self):
-        return self.name
+        return f'{self.name} ({self.measurement_unit})'
 
 
 class RecipeIngredient(models.Model):
